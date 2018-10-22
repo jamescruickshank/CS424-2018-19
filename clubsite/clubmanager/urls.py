68adb984793715a02679e80d4d1ca22f682b169d
jamescruickshank <james.cruickshank@nuigalway.ie>
2018-10-22 14:02:11 +0100

    new lecture

--- clubsite/clubmanager/urls.py
+++ clubsite/clubmanager/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('member/id/<int:member_id>', views.member),
+    path('member/id/<int:member_id>', views.member, name="member_profile"),
+    path('members', views.member_list),
 ]
--- clubsite/clubmanager/views.py
+++ clubsite/clubmanager/views.py
@@ -12,16 +12,15 @@ from clubmanager.models import Member
 def member(request,member_id):
     member = Member.objects.get(id=member_id)
 
-    #return HttpResponse('%s %s'%(member.first_name,member.last_name))
     response = render(request,'clubmanager/member_detail.html',{
         'member':member
         })
-    #embed()
     return response
 
 def member_list(request):
     members = Member.objects.all()
-    embed()
 
-
-    return HttResponse('This should be a list of members')
+    response=render(request,'clubmanager/member_list.html',{
+            'members':members
+    })
+    return response
